869452c50db9386d691f17041fb566a38871a5b0
DanTanAtAims <d.tan@aims.gov.au>
2025-08-14 10:56:06 +1000

    add relative coral cover metrics

--- src/ReefMetrics.jl
+++ src/ReefMetrics.jl
@@ -1,10 +1,12 @@
 module ReefMetrics
 
 include("aggregation.jl")
-include("conversion.jl")
+include("conversions.jl")
 include("metrics.jl")
 
 export coral_diversity, coral_evenness, absolute_shelter_volume, 
     relative_shelter_volume
+
+export rhc_to_rrc, rrc_to_rhc
 
 end
